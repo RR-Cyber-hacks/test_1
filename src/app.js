5069d26b5ebd10da95b8ced27e092bc7876ca1d4
RR <rizwan345300@gmail.com>
2024-09-22 16:50:18 +0500

    pakages are added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
  const express = require('express');
  const app = express();
+ const port = process.env.PORT || 4000;
 
 
  app.use(express.json());
@@ -9,4 +10,8 @@
 
  app.get('/', (req, res) => {
     res.send('Welcome');
+ });
+
+ app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
  });
