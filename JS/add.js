391741da8fc751ac57941c41420227693cc308bc
RR <rizwan345300@gmail.com>
2024-09-22 16:08:51 +0500

    node 1st commit

--- JS/add.js
+++
@@ -1,9 +0,0 @@
-const APP = () => {
-    return (
-        <div>
-            <h1>Welcome to my React App!</h1>
-            <p>This is a simple React component.</p>
-            <img src="" alt="" />
-        </div>
-    )
-}
