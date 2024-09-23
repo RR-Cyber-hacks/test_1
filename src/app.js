00ee18dc4de76a372eb9e7fd2157e164564e4765
RR <rizwan345300@gmail.com>
2024-09-23 13:49:10 +0500

    install nodemon

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
- const express = require('express');
- const app = express();
- const port = process.env.PORT || 4000;
+require('./db/conn');
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 4000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
- app.use(express.json());
- app.use(express.urlencoded({ extended: true }));
- 
- app.use(express.static('public'));
+// Static path
+const staticPath = path.join(__dirname, '../public');
+app.use(express.static(staticPath));
 
- app.get('/', (req, res) => {
-    res.send('Welcome');
- });
+console.log(staticPath);
 
- app.listen(port, () => {
+app.get('/', (req, res) => {
+    // res.render('index.html');
+    res.send('Bis');
+});
+
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
- });
+});
