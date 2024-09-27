25c6ef4676276efc22c15f2aeed9ea75ed4a6ee8
RR <rizwan345300@gmail.com>
2024-09-27 12:30:32 +0500

    new commit

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const hbs = require('hbs');
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 // Static path
 const staticPath = path.join(__dirname, '../public');
@@ -18,15 +18,35 @@ const partialPath = path.join(__dirname, '../templates/partials');
 app.use(express.static(staticPath));
 app.set('view engine', 'hbs');
 app.set('views', dynamicPath);
-hbs.registerPartials(partialPath); 
+hbs.registerPartials(partialPath);
 // Dynamic path
 
-console.log(staticPath);
-
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     res.render('login');
     // res.send('Bis');
 });
+
+app.get('/register', async (req, res) => {
+    try {
+        res.render('register');
+    } catch (e) {
+        res.status(404).send('Invalid');
+    }
+});
+
+app.get('/home', async (req, res) => {
+    res.render('home');
+    // res.send('Bis');
+});
+
+app.post('/home', async (req, res) => {
+    try {
+        console.log(req.body.username);
+        res.send(req.body.username);
+    }catch (e) {}
+})
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
